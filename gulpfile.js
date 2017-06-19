15ee7897506fa47399f3a68795d44cc2f477f191
Kyle Conrad <kyle@kyleconrad.com>
2017-06-19 14:23:25 -0400

    Set up base gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,90 @@
+require('events').EventEmitter.prototype._maxListeners = 100;
+
+
+var gulp = require('gulp'),
+
+	// Server & BrowserSync
+	browserSync = require('browser-sync').create(),
+
+	// Required for development
+	sass = require('gulp-sass'),
+	sourcemaps = require('gulp-sourcemaps'),
+	fileinclude = require('gulp-file-include'),
+	rename = require('gulp-rename');
+
+
+
+
+
+// =================================================================================== //
+// Development
+// =================================================================================== //
+
+
+// Server initiation, BrowserSync, and watch tasks (opens site in browser)
+gulp.task('default', ['html-build', 'sass'], function() {
+    browserSync.init(null, {
+		server: {
+			baseDir: './dev'
+		},
+		host: 'localhost'
+    });
+
+	gulp.watch('./dev/**/*.html', ['html-watch']);
+    gulp.watch('./dev/sass/**/*.scss', ['sass']);
+    gulp.watch('./dev/js/**/*.js').on('change', browserSync.reload);
+    gulp.watch('./dev/images/**/*').on('change', browserSync.reload);
+});
+
+
+// Compile all HTML from templates and includes
+gulp.task('html-build', function() {
+	return gulp.src('./dev/templates/index.tpl.html')
+		.pipe(fileinclude({
+			prefix: '@@',
+			basepath: './dev/templates/'
+		}))
+		.pipe(rename({
+ 			extname: ''
+		 }))
+ 		.pipe(rename({
+ 			extname: '.html'
+ 		}))
+		.pipe(gulp.dest('./dev'));
+});
+
+
+// Ensure HTML is compiled before reloading browsers
+gulp.task('html-watch', ['html-build'], function (done) {
+	browserSync.reload();
+	done();
+});
+
+
+// Run SASS compiling and reloading
+gulp.task('sass', function() {
+    return gulp.src('./static/sass/*.scss')
+	    .pipe(sourcemaps.init())
+        .pipe(sass({
+        	errLogToConsole: true
+        }))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./dev/css'))
+        .pipe(browserSync.stream());
+});
+
+
+
+
+
+// =================================================================================== //
+// Build
+// =================================================================================== //
+
+
+
+
+
+// =================================================================================== //
+// Deployment
+// =================================================================================== //
